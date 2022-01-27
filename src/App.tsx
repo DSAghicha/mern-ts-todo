bd7daf6d1c11568f3e82a29049d6df261dd35618
DSAghicha <dsaghicha211@gmail.com>
2022-01-27 23:14:08 +0530

    init: Created Source folder

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.scss'
+import { FC } from "react";
+
+const App: FC = () => {
+    return(
+        <div className="App">
+            <h1>Hello World</h1>
+        </div>
+    )
+};
+
+export default App;
