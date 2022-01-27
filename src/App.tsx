aefaf5a054a6bb6fe53e787ebed3cf2664bde152
DSAghicha <dsaghicha211@gmail.com>
2022-01-28 00:42:14 +0530

    feat: Initial Designs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import './App.scss'
 import { FC } from "react";
+import { ReactComponent as LightModeIcon } from './assets/icons/light-mode.svg'
+import { ReactComponent as DarkModeIcon } from './assets/icons/dark-mode.svg'
+
 
 const App: FC = () => {
     return(
-        <div className="App">
-            <h1>Hello World</h1>
+        <div className='App'>
+            <div className="bg-banner"></div>
+            <section className="main-container">
+                <div className="heading">
+                    <h1>TODO</h1>
+                    <div id="toggle-btn">
+                        <LightModeIcon />
+                        <DarkModeIcon />
+                    </div>
+                </div>
+                <div className="input-cr">
+                        <input type="text" name="todo-task" id="todo-task" placeholder='Create a new ToDo'/>
+                        <input type="submit" value='Add' />
+                </div>
+
+                <div className="task-display">
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                </div>
+
+                <div className="footer"></div>
+            </section>
         </div>
     )
 };
